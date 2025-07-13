8cd8824ac453fd4e776c257099ba1670ad17d3d3
DineshJoshi2005 <djdineshjoshi2005@gmail.com>
2025-07-13 14:06:35 +0530

    Node Server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const http = require('http');
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        res.end("This is the home page");
+    }
+    if(req.url == "/about"){
+        res.end("This is an about page ");
+    }
+    if(req.url == "/profile"){
+        res.end("This is an profile page");
+    }
+});
+
+server.listen(3000);
